7b62125fb2d242f8e30049212410086775f5d333
ricoter <rico.vanmidde@gmail.com>
2021-01-23 14:31:44 +0100

    step! returns pos

--- DynamicalSystems/RosslerModel.jl
+++ DynamicalSystems/RosslerModel.jl
@@ -6,16 +6,17 @@ Implementation of the Rossler system by Rico van Midde, 2021
 
 Base.@kwdef mutable struct Rossler
     dt::Float64 = 0.02
-    a::Float64 = 0.2
-    b::Float64 = 0.2
-    c::Float64 = 0.2
+    a::Float64 = 0.1
+    b::Float64 = 0.1
+    c::Float64 = 14
     x::Float64 = 1
     y::Float64 = 1
     z::Float64 = 1
 end
 
-function step!(r::Rossles)
+function step!(r::Rossler)
     dx = - r.y - r.z;           r.x += r.dt * dx
     dy = r.x + r.z;             r.y += r.dt * dy
     dz = b + r.z * (r.x - c);   r.z += r.dt * dz
+    return r.x, r.y, r.z
 end
