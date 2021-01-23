b5172b6d454bd521af46f198e64ba491d9ac28a8
ricoter <rico.vanmidde@gmail.com>
2021-01-23 14:35:12 +0100

    try out, not working at all

--- Examples/3DimAttractors.jl
+++ Examples/3DimAttractors.jl
@@ -1,29 +1,51 @@
-using Plots
+using Plots, Random
 
 include("../DynamicalSystems/LorenzAttractor.jl")
 include("../DynamicalSystems/RosslerModel.jl")
 
 styles = Dict(
-    "Lorenz Attractor" => (Lorenz,  )
-    "Rossler Model"    => (Rossler, )
+    "Lorenz Attractor" => (Lorenz,  (a=0.1, b=0.1, c=14)),
+    "Rossler Model"    => (Rossler, (σ=10, β=8/3, ρ=28))
 )
 
-x, y, z = 3 * randn(3) 
-attractor = Lorenz(x=x,y=y,z=z)
+dt = 0.02
+attractor = Lorenz(dt=dt)
+
+# l = @layout([a [b; c; d]])
+
+
 
 # initialize a 3D plot with 1 empty series
-plt = plot3d(
+p1 = plot3d(
     1,
     xlim = (-30, 30),
     ylim = (-30, 30),
     zlim = (0, 60),
-    background = "Black",
+    background = :Black,
     title = "Lorenz Attractor",
     marker = 2,
 )
 
+
 # build an animated gif by pushing new points to the plot, saving every 10th frame
-@gif for i=1:1500
+@gif for i=1:50
+
+
+    l = @layout([a [b; c; d]])
+
+
+    plt = plot(layout = l, st = [:p1 :line :line :line], leg = false, ticks = nothing, border = :none)
+    
+    
     step!(attractor)
-    push!(plt, attractor.x, attractor.y, attractor.z)
+
+    xyz = [attractor.x, attractor.y, attractor.z]
+
+    plot3d!(xyz, subplot = plt[1])
+
+    plot!(xyz)
+    # for j =1:3
+    #     plot!([xyz[j]], subplot =plt[j])
+    # end
+
 end every 10
