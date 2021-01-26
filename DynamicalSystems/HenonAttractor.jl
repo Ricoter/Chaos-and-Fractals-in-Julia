0a6a96917d6aa667563c757a3e9950fd469442b8
ricoter <rico.vanmidde@gmail.com>
2021-01-26 21:01:06 +0100

    description/fix/todo

---
+++ DynamicalSystems/HenonAttractor.jl
@@ -0,0 +1,33 @@
+#=
+
+Implementation of the Hénon system by Rico van Midde, 2021
+
+
+"The Hénon map , sometimes called Hénon-Pomeau attractor/map, is a discrete-time dynamical system. 
+It is one of the most studied examples of dynamical systems that exhibit chaotic behavior. 
+The Hénon map takes a point (xn, yn) in the plane and maps it to a new point. 
+
+                            {x[t+1] = 1 - a * x[t]^2 + y[t]
+                            {y[t+1] = b * x[t]
+
+The map depends on two parameters, a and b, which for the classical Hénon map have values of a = 1.4 and b = 0.3. 
+For the classical values the Hénon map is chaotic. For other values of a and b the map may be chaotic, intermittent, 
+or converge to a periodic orbit. An overview of the type of behavior of the map at different parameter values may be 
+obtained from its orbit diagram." ~ (wikipedia)
+=#
+
+# TODO Add examples
+
+Base.@kwdef mutable struct Henon
+    dt::Float64 = 0.02
+    a::Float64 = 1.4
+    b::Float64 = 0.3
+    x::Float64 = 1
+    y::Float64 = 1
+end
+
+function step!(h::Henon)
+    dx = 1 - h.a * h.x^2 + h.y;     h.x += h.dt * dx
+    dy = h.b * h.x;                 h.y += h.dt * dy
+    return h.x, h.y
+end
