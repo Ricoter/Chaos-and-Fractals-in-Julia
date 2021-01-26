ac7a973b321cd4d87b7730c9ef8ae11730f5fc37
ricoter <rico.vanmidde@gmail.com>
2021-01-26 17:29:55 +0100

    clean

--- Examples/3DimAttractorsMakie.jl
+++ Examples/3DimAttractorsMakie.jl
@@ -1,38 +1,34 @@
 #=
-Simple example to show a 3d attractor in a combined plot.
+Example: plots in Makie of 3D dynamical system
 =#
 
-
-# TODO add titles
-# TODO combine models
 # TODO make this into an animation
-
 
 using GLMakie, AbstractPlotting
 
 include("../DynamicalSystems/LorenzAttractor.jl")
 include("../DynamicalSystems/RosslerModel.jl")
 
+# 
 styles = Dict(
     "Lorenz Attractor" => [Lorenz, (σ=10, β=8/3, ρ=28)],
     "Rossler Model"    => [Rossler, (a=0.1, b=0.1, c=14)]
 )
 
-dt = 0.01
+dt = 0.02
 name = "Lorenz Attractor"
 # name = "Rossler Model"
 
 model, kwargs = styles[name]
 attractor = model(dt=dt; kwargs...)
-xyz = [step!(attractor) for _=1:20000]
+xyz = [step!(attractor) for _=1:10000]
 x = [x for (x,_,_) in xyz]
 y = [y for (_,y,_) in xyz]
 z = [z for (_,_,z) in xyz]
 
 f = Figure()
 
-
-lines(f[1:3,1], xyz, markersize=0.5, color=(:blue, 0.8), axis = (title="aaa",))
+lines(f[1:3,1], xyz, markersize=0.5, color=(:blue, 0.8))
 lines(f[1,2], x, axis = (ylabel = "x",), color=(:green, 0.8))
 lines(f[2,2], y, axis = (ylabel = "y",), color=(:purple, 0.8))
 lines(f[3,2], z, axis = (ylabel = "z", xlabel = "t"), color=(:red, 0.8))
