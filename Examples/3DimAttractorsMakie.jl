48309cdbab2877d51cacb6fd9fda83a968aa4e43
ricoter <rico.vanmidde@gmail.com>
2021-01-27 01:47:52 +0100

    add Chua, needs to be fixed

--- Examples/3DimAttractorsMakie.jl
+++ Examples/3DimAttractorsMakie.jl
@@ -8,20 +8,23 @@ using GLMakie, AbstractPlotting
 
 include("../DynamicalSystems/LorenzAttractor.jl")
 include("../DynamicalSystems/RosslerModel.jl")
+include("../DynamicalSystems/ChuaAttractor.jl")
 
 # 
 styles = Dict(
+    "Chua Circuit"     => [Chua, (α=10, β=14.87, m0=-0.68, m1=-1.27)],
     "Lorenz Attractor" => [Lorenz, (σ=10, β=8/3, ρ=28)],
-    "Rossler Model"    => [Rossler, (a=0.1, b=0.1, c=14)]
+    "Rossler Model"    => [Rossler, (a=0.1, b=0.1, c=14)],
 )
 
-dt = 0.02
+dt = 0.00001
 name = "Lorenz Attractor"
 # name = "Rossler Model"
+name = "Chua Circuit"
 
 model, kwargs = styles[name]
 attractor = model(dt=dt; kwargs...)
-xyz = [step!(attractor) for _=1:10000]
+xyz = [step!(attractor) for _=1:10000000][begin:1000:end]
 x = [x for (x,_,_) in xyz]
 y = [y for (_,y,_) in xyz]
 z = [z for (_,_,z) in xyz]
