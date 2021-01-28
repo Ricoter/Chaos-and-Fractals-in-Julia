c6350b994d1011c3c16256b6943469e519ef3a32
ricoter <rico.vanmidde@gmail.com>
2021-01-28 10:29:58 +0100

    clean

--- DynamicalSystems/PWLDuffingAttractor.jl
+++ DynamicalSystems/PWLDuffingAttractor.jl
@@ -23,11 +23,14 @@ Base.@kwdef mutable struct PWLDuffing
 end
 
 function step!(s::PWLDuffing)
+    # differences
     dx = s.x + s.y
     dy = s.y + (-s.m1 * s.x - (s.m0 - s.m1) / 2(abs(s.x+1)-abs(s.x-1)) - s.e * s.y + s*γ*cos(s.ω*s.t))
 
+    # update
     s.x += s.dt * dx
     s.y += s.dt * dy
     s.t += dt
+    
     return l.x, l.y
 end
