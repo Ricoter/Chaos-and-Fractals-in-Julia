367031f65ce6c03cffe7f4f3ed5baf3a4115391c
ricoter <rico.vanmidde@gmail.com>
2021-01-31 17:52:55 +0100

    add inplace

--- IterativeMethods/RK4.jl
+++ IterativeMethods/RK4.jl
@@ -3,16 +3,30 @@ Runge-kutte method for iteration
 
 RK4 is a fourth-order method. This means that the local truncation error is of the order O(h^5) 
 while the global truncation error (total accumulated error) is of the order O(h^4)
+
 =#
 
 #TODO add example
 
 function RK4(f, t, h, y)
-    k1 = f(t, y)
-    k2 = f(t + h / 2, y + h * k1 / 2)
-    k3 = f(t + h / 2, y + h * k2 / 2)
-    k4 = f(t + h, y + h * k3)
+    k1 = f(t, y)                        # Slope at beginning of the interval
+    k2 = f(t + h / 2, y + h * k1 / 2)   # Slope at midpoint (y and k1)
+    k3 = f(t + h / 2, y + h * k2 / 2)   # Slope at midpoint (y and k2)
+    k4 = f(t + h, y + h * k3)           # Slope at end (y and k3)
+    
+    # Update
     t += h
-    y += h/6 * (k1 + 2k2 + 2k3 + k4)
+    y += h * (k1 + 2k2 + 2k3 + k4) / 6  # Weighted average
     return y, t
+end
+
+function RK4!(f, t, h, y)
+    k1 = f(t, y)                        # Slope at beginning of the interval
+    k2 = f(t + h / 2, y + h * k1 / 2)   # Slope at midpoint (y and k1)
+    k3 = f(t + h / 2, y + h * k2 / 2)   # Slope at midpoint (y and k2)
+    k4 = f(t + h, y + h * k3)           # Slope at end (y and k3)
+    
+    # Update
+    t += h
+    y += h * (k1 + 2k2 + 2k3 + k4) / 6  # Weighted average
 end
